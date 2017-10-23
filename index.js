ae0076b12b73c368582cae8d656100346d6e84e9
Chris Swimmer <chris@Chriss-MacBook-Air.local>
2017-10-23 12:38:03 -0700

    testing upload of local files

---
+++ index.js
@@ -0,0 +1,7 @@
+$('.navbar  li').click(function(e) {
+    $('.navbar li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+  });
