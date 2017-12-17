be8e59e93002343fcc1481b169d7bc716ad8b7af
Chris Swimmer <chrisrswimmer@gmail.com>
2017-12-17 08:28:51 -0800

    color for coming soon

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ setInterval(function(){
  switch(change){
     case 0:
       adjective = "attractive";
+      document.getElementById('adjective').style.color = "#01FEFF";
       break;
     case 1:
       adjective = "interesting";
+      document.getElementById('adjective').style.color = "#EEFF02";
       break;
     case 2:
       adjective = "exciting";
+      document.getElementById('adjective').style.color = "#FFFD03";
       break;
     case 3:
       adjective = "accessible";
+      document.getElementById('adjective').style.color = "#01FEFF";
       break;
     case 4:
       adjective = "successful";
+      document.getElementById('adjective').style.color = "#EEFF02";
       break;
     case 5:
       adjective = "transformative";
+      document.getElementById('adjective').style.color = "#FFFD03";
       break;
   }
   
