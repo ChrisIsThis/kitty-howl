e82754f4b1e2cac38ad574b628eaa1b5503a6b36
Chris Swimmer <chrisrswimmer@gmail.com>
2017-12-17 08:31:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ setInterval(function(){
  switch(change){
     case 0:
       adjective = "attractive";
-      document.getElementById('adjective').style.color = "#01FEFF";
+      document.getElementById('coming-soon').style.color = "#01FEFF";
       break;
     case 1:
       adjective = "interesting";
-      document.getElementById('adjective').style.color = "#EEFF02";
+      document.getElementById('coming-soon').style.color = "#EEFF02";
       break;
     case 2:
       adjective = "exciting";
-      document.getElementById('adjective').style.color = "#FFFD03";
+      document.getElementById('coming-soon').style.color = "#FFFD03";
       break;
     case 3:
       adjective = "accessible";
-      document.getElementById('adjective').style.color = "#01FEFF";
+      document.getElementById('coming-soon').style.color = "#01FEFF";
       break;
     case 4:
       adjective = "successful";
-      document.getElementById('adjective').style.color = "#EEFF02";
+      document.getElementById('coming-soon').style.color = "#EEFF02";
       break;
     case 5:
       adjective = "transformative";
-      document.getElementById('adjective').style.color = "#FFFD03";
+      document.getElementById('coming-soon').style.color = "#FFFD03";
       break;
   }
   
