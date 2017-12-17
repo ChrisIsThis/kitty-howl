d0adbf47d6771b47693375e52c10fc061ce5bb5b
Chris Swimmer <chrisrswimmer@gmail.com>
2017-12-17 07:28:43 -0800

    a little bit o fun

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-$('.navbar  li').click(function(e) {
-    $('.navbar li.active').removeClass('active');
-    var $this = $(this);
-    if (!$this.hasClass('active')) {
-        $this.addClass('active');
-    }
-  });
+var change = 0;
+var adjective = '';
+setInterval(function(){
+ switch(change){
+    case 0:
+      adjective = "attractive";
+      break;
+    case 1:
+      adjective = "interesting";
+      break;
+    case 2:
+      adjective = "exciting";
+      break;
+    case 3:
+      adjective = "accessible";
+      break;
+    case 4:
+      adjective = "successful";
+      break;
+  }
+  
+  document.getElementById("adjective").innerHTML = adjective;
+  if(change === 4){
+    change = 0;
+  } else {
+    change++;
+  }
+  
+},3000)
